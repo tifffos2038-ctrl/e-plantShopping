e40f107e9fcaa08e389f1ea805f5b0de3320bc49
tifffos2038-ctrl <tifffos2038@gmail.com>
2026-04-02 09:36:06 -0500

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,7 +3,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [showPlants, setShowPlants] = useState(false); 
 
     const plantsArray = [
         {
